c38f02b83be6105c597ddc6ec01e2a0da920b09c
soraping <soraping@163.com>
2015-08-19 23:11:14 +0800

    jade

--- jade/app.js
+++ jade/app.js
@@ -9,14 +9,14 @@ http.createServer(function (req, res) {
 	// var html = fn({course:'shengchengde compile html'});
 
 	//2.compileFile
-	var fn = jade.compileFile('demo002.jade',{});
-	var html = fn({course:'shengchengde compile html'});
+	//var fn = jade.compileFile('demo002.jade',{});
+	//var html = fn({course:'shengchengde compile html'});
 
 	//3.jade.render
 	//var html = jade.render('div #{course}',{course:'shengchengde render html'});
 
 	//5.jade.renderFile
-	//var html = jade.renderFile('demo001.jade',{course:'shengchengde renderFile html',pretty:true})
+	var html = jade.renderFile('demo001.jade',{course:'shengchengde renderFile html',pretty:true});
 
 	res.end(html);
 }).listen(1337,'127.0.0.1');
---
+++ jade/runtime.js
@@ -0,0 +1,7 @@
+function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+
+buf.push("<!--Created by Administrator on 2015/8/19.--><div><h3>jade runtime call</h3><p>this is from jade pre-compile</p></div>");;return buf.join("");
+}
