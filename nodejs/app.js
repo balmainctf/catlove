8bfec2e430fed004c52b0575f64d50cc7a5ec444
caoshiping <caoshiping@qianmi.com>
2015-08-24 19:28:59 +0800

    add node pro

---
+++ nodejs/app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function(req,res){
+	//200 status
+	res.writeHead(200,{'Content-Type':"text/plain"});
+	res.end('Hello World\n');
+}).listen(5858,'127.0.0.1');
+console.log('Server running at port 5858');
