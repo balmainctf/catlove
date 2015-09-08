60b00a8a5ffa102f0b60648826c685ba780f7088
soraping <soraping@163.com>
2015-09-08 20:03:02 +0800

    add

--- react/app/js/main.js
+++ react/app/js/main.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2015/9/8.
  */
 var React = require('react');
-var QuestionApp = require('./components/ShowAddButton.js');
+var QuestionApp = require('./components/QuestionApp.js');
 
 var mainCom = React.render(
 	<QuestionApp />,
