9385e36a447f81c7389c3042b4bf54d05729fc6d
soraping <soraping@163.com>
2015-09-16 20:11:07 +0800

    'flux'

---
+++ flux/chat/webpack.config.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Created by Administrator on 2015/9/16.
+ */
+var webpack = require('webpack');
+//处理公用部分的js,并合并成一个common.js
+//var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+
+module.exports = {
+    entry: './js/app.js',
+    output:{
+        path: './build',
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'jsx-loader' } // loaders 可以接受 querystring 格式的参数
+        ]
+    }
+};
