1872f9b8c9e5361d3d101bb515532a7e829c90dd
soraping <soraping@163.com>
2016-07-21 00:57:02 +0800

    ceshi

--- nodejs/meirentu/es6_index_2.js
+++ nodejs/meirentu/es6_index_2.js
@@ -8,10 +8,12 @@ let cheerio = require('cheerio');
 let request = require('./request.js');
 
 let startPageNum = 1;
-let lastPageNum = 2;
-let reqUrl = 'http://www.pic177.com/html/2015/07/923717.html';
+let lastPageNum = 1;
+let reqUrl = 'http://www.twfun.info/html/2014/03/46099.html';
 
-let getPage = async (url)=>{
+let errorPage = [];
+
+let getPage = async (url,pageNum)=>{
     let res;
     try{
         console.log('请求URL===》'+url);
@@ -20,7 +22,8 @@ let getPage = async (url)=>{
             timeout:5000
         });
     }catch(e){
-        console.error('请求页面异常=====>',e);
+        console.error('第'+pageNum+'页请求页面异常');
+        errorPage.push(pageNum);
         return;
     }
     if(res && res.status == 200){
@@ -30,7 +33,7 @@ let getPage = async (url)=>{
         let $ = cheerio.load(body);
         //解析指定位置图片数组
         let meiren = $('.entry-content p img').toArray();
-        await acquireData(meiren);
+        await acquireData(meiren,pageNum);
     }
 
 };
@@ -39,10 +42,10 @@ let getPage = async (url)=>{
  * 解析数组对象
  * @param imgArr
  */
-let acquireData = async (imgArr) => {
+let acquireData = async (imgArr,pageNum) => {
     for(let i=0,len=imgArr.length;i<len;i++){
         let imgSrc = imgArr[i].attribs.src;
-        let filename = parseUrlForFileName(imgSrc);
+        let filename = parseUrlForFileName(imgSrc,pageNum,i);
         console.log('开始下载图片'+filename);
         await downloadImg(imgSrc,filename);
         console.log(filename+"下载完成");
@@ -53,8 +56,9 @@ let acquireData = async (imgArr) => {
  * @param address
  * @returns {*}
  */
-let parseUrlForFileName = (address) => {
-    let filename = path.basename(address);
+let parseUrlForFileName = (address,pageNum,index) => {
+    //let filename = path.basename(address);
+    let filename = "p"+pageNum+'-'+index+'.jpg';
     return filename;
 };
 
@@ -83,12 +87,18 @@ let downloadImg = async (url, filename) => {
  */
 var queryPage = async (startPageNum,lastPageNum,reqUrl) => {
     for(let i=startPageNum;i<lastPageNum+1;i++){
-        let url = reqUrl + '/' + i;
+        let url = reqUrl;
+        if(i > 1){
+            url = reqUrl + '/' + i;
+        }
         console.log('第' + i + '页开始');
-        await getPage(url);
+        await getPage(url,i);
         console.log('第' + i + '页结束');
     }
     console.log('爬取结束');
+    if(errorPage.length){
+        console.log('失败请求页面======>'+errorPage.toString());
+    }
 };
 
 /**
